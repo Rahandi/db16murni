bee3ea6e6329ffbce80b7da7920af357bd0d52a7
raldo <raldo0104@gmail.com>
2018-05-29 16:59:49 +0700

    ws tak hapus

--- register.php
+++ register.php
@@ -1,164 +1,110 @@
-<?php
-include('config.php');
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
-    } else{
-        $sql = "SELECT id FROM users WHERE name = ?";
-        if($stmt = mysqli_prepare($db, $sql)){
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            $param_username = trim($_POST["username"]);
-            if(mysqli_stmt_execute($stmt)){
-                mysqli_stmt_store_result($stmt);
-                if(mysqli_stmt_num_rows($stmt) == 1){
-                    $username_err = "This username is already taken.";
-                } else{
-                    $username = trim($_POST["username"]);
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-        }
-        mysqli_stmt_close($stmt);
-    }
-    if(empty(trim($_POST['password']))){
-        $password_err = "Please enter a password.";
-    } elseif(strlen(trim($_POST['password'])) < 8){
-        $password_err = "Password must have atleast 8 characters.";
-    } else{
-        $password = trim($_POST['password']);
-    }
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = 'Please confirm password.';
-    } else{
-        $confirm_password = trim($_POST['confirm_password']);
-        if($password != $confirm_password){
-            $confirm_password_err = 'Password did not match.';
-        }
-    }
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-        $sql = "INSERT INTO users (name, password, email) VALUES (?, ?, 'kosong')";
-        if($stmt = mysqli_prepare($db, $sql)){
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-            if(mysqli_stmt_execute($stmt)){
-                header("location: login.php");
-            } else{
-                die(mysqli_error($db));
-            }
-            mysqli_stmt_close($stmt);
-        } else{
-            die(mysqli_error($db));
-        }
-    }
-    mysqli_close($db);
-}
-?>
+<?php 
+include('config.php'); 
+$username = $password = $confirm_password = ""; 
+$username_err = $password_err = $confirm_password_err = ""; 
+if($_SERVER["REQUEST_METHOD"] == "POST"){ 
+    if(empty(trim($_POST["username"]))){ 
+        $username_err = "Please enter a username."; 
+    } else{ 
+        $sql = "SELECT id FROM users WHERE name = ?"; 
+        if($stmt = mysqli_prepare($db, $sql)){ 
+            mysqli_stmt_bind_param($stmt, "s", $param_username); 
+            $param_username = trim($_POST["username"]); 
+            if(mysqli_stmt_execute($stmt)){ 
+                mysqli_stmt_store_result($stmt); 
+                if(mysqli_stmt_num_rows($stmt) == 1){ 
+                    $username_err = "This username is already taken."; 
+                } else{ 
+                    $username = trim($_POST["username"]); 
+                } 
+            } else{ 
+                echo "Oops! Something went wrong. Please try again later."; 
+            } 
+        } 
+        mysqli_stmt_close($stmt); 
+    } 
+    if(empty(trim($_POST['password']))){ 
+        $password_err = "Please enter a password."; 
+    } elseif(strlen(trim($_POST['password'])) < 8){ 
+        $password_err = "Password must have atleast 8 characters."; 
+    } else{ 
+        $password = trim($_POST['password']); 
+    } 
+    if(empty(trim($_POST["confirm_password"]))){ 
+        $confirm_password_err = 'Please confirm password.'; 
+    } else{ 
+        $confirm_password = trim($_POST['confirm_password']); 
+        if($password != $confirm_password){ 
+            $confirm_password_err = 'Password did not match.'; 
+        } 
+    } 
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){ 
+        $sql = "INSERT INTO users (name, password, email) VALUES (?, ?, 'kosong')"; 
+        if($stmt = mysqli_prepare($db, $sql)){ 
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password); 
+            $param_username = $username; 
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash 
+            if(mysqli_stmt_execute($stmt)){ 
+                header("location: login.php"); 
+            } else{ 
+                echo "Something went wrong. Please try again later."; 
+            } 
+            mysqli_stmt_close($stmt); 
+        } else{ 
+            die(mysqli_error($db)); 
+        } 
+    } 
+    mysqli_close($db); 
+} 
+?> 
+ 
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <link rel="stylesheet" type="text/css" href="css/register.css">
+    <meta charset="UTF-8"> 
+    <title>Sign Up</title> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css"> 
+    <style type="text/css"> 
+        body{ font: 14px sans-serif; } 
+        .wrapper{ width: 350px; padding: 20px; } 
+    </style> 
+</head> 
+<body> 
+<img src="img/bg1.png" id="gbr">
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags-->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="au theme template">
-    <meta name="author" content="Hau Nguyen">
-    <meta name="keywords" content="au theme template">
-
-    <!-- Title Page-->
-    <title>Register</title>
-
-    <!-- Fontfaces CSS-->
-    <link href="css/font-face.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
-    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
-
-    <!-- Bootstrap CSS-->
-    <link href="vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
-
-    <!-- Vendor CSS-->
-    <link href="vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
-    <link href="vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
-    <link href="vendor/wow/animate.css" rel="stylesheet" media="all">
-    <link href="vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
-    <link href="vendor/slick/slick.css" rel="stylesheet" media="all">
-    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
-    <link href="vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
-
-    <!-- Main CSS-->
-    <link href="css/theme.css" rel="stylesheet" media="all">
-
-</head>
-
-<body class="animsition">
-    <div class="page-wrapper">
-        <div class="page-content--bge5">
-            <div class="container">
-                <div class="login-wrap">
-                    <div class="login-content">
-                        <div class="login-form">
-                            <form action="" method="post">
-                                <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                                    <label>Username</label>
-                                    <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
-                                    <span class="help-block"><?php echo $username_err; ?></span>
-                                </div>
-                                <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                                    <label>Password</label>
-                                    <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
-                                    <span class="help-block"><?php echo $password_err; ?></span>
-                                </div>
-                                <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-                                    <label>Confirm Password</label>
-                                    <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
-                                    <span class="help-block"><?php echo $confirm_password_err; ?></span>
-                                </div>
-                                <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit">register</button>
-                            </form>
-                            <div class="register-link">
-                                <p>
-                                    Already have account?
-                                    <a href="login.php">Sign In</a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
-
-    <!-- Jquery JS-->
-    <script src="vendor/jquery-3.2.1.min.js"></script>
-    <!-- Bootstrap JS-->
-    <script src="vendor/bootstrap-4.1/popper.min.js"></script>
-    <script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
-    <!-- Vendor JS       -->
-    <script src="vendor/slick/slick.min.js">
-    </script>
-    <script src="vendor/wow/wow.min.js"></script>
-    <script src="vendor/animsition/animsition.min.js"></script>
-    <script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
-    </script>
-    <script src="vendor/counter-up/jquery.waypoints.min.js"></script>
-    <script src="vendor/counter-up/jquery.counterup.min.js">
-    </script>
-    <script src="vendor/circle-progress/circle-progress.min.js"></script>
-    <script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
-    <script src="vendor/chartjs/Chart.bundle.min.js"></script>
-    <script src="vendor/select2/select2.min.js">
-    </script>
-
-    <!-- Main JS-->
-    <script src="js/main.js"></script>
-
-</body>
-
+<div class="wrapper">
+	<div class="reg">
+		<div id="inside">
+	    	<img src="img/logo.png" height="180" width="220">
+			<h2 style="color: white"><center>Sign Up</center></h2> 
+    		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
+        	<div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>"> 
+            	<label style="color: white">Username</label> 
+            	<input type="text" name="username"class="form-control" value="<?php echo $username; ?>"> 
+            	<span class="help-block"><?php echo $username_err; ?></span> 
+       	 	</div> 
+       	 	<div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>"> 
+            	<label style="color: white">Password</label> 
+            	<input type="password" name="password" class="form-control" value="<?php echo $password; ?>"> 
+            	<span class="help-block"><?php echo $password_err; ?></span> 
+        	</div> 
+        	<div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>"> 
+            	<label style="color: white" >Confirm Password</label> 
+            	<input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>"> 
+            	<span class="help-block"><?php echo $confirm_password_err; ?></span> 
+       	 	</div> 
+        	<div class="form-group"> 
+            	<input style="float: left; background-color: #4b7bec" type="submit" class="btn btn-primary" value="Submit"> 
+            	<input style="float: right; background-color: #ffb142; color: white" type="reset" class="btn btn-default" value="Reset"> 
+        	</div> 
+        	<br><br><br>
+        	<p style="color: white">Already have an account? <a href="login.php" style="color: #4b6584">Login here</a>.</p> 
+    		</form>	
+		</div>
+	
+	</div>
+     
+</div> 
+</body> 
 </html>
-<!-- end document-->
